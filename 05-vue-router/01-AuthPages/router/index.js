ccfa11c6e7d0b117a666bb804ff69a75cd7dc356
Сергей <skaluzhni@sutochno.ru>
2022-12-09 21:14:32 +0300

    05-vue-router

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -1,6 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import PageIndex from "../views/PageIndex";
 
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/01-AuthPages'),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: PageIndex
+    },
+    {
+      path: '/login ',
+      name: 'login ',
+      component: () => import('../views/PageLogin')
+    },
+    {
+      path: '/register ',
+      name: 'register ',
+      component: () => import('../views/PageRegister')
+    },
+  ],
 });
